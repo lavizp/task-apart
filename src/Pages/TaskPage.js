f0842aaf1b81be42f05e556b8e131fcd48a86d0b
Laviz-pand <pandeylaviz@gmail.com>
2022-10-27 07:20:30 +0545

    Updates

--- src/Pages/TaskPage.js
+++ src/Pages/TaskPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import NavbarLeft from '../Components/NavbarLeft'
 import Navbar from '../Components/Navbar'
@@ -7,14 +7,21 @@ import TasksContainer from '../Components/TasksContainer'
 import NotesTab from '../Components/NotesTab'
 import AddTask from '../Components/AddTask'
 import { useAuth } from '../FIrebase/authContext'
+import { useNavigate } from 'react-router-dom'
 export default function TaskPage() {
   const{currentUser} = useAuth();
   const {tasks} = useSelector((state)=> state.taskSlice)
-
+  const navigate = useNavigate();
     const[isAddTaskVisible, setAddTask] = useState(false)
     function displayAddTask(){
       setAddTask(prev =>!prev);
     }
+
+    useEffect(()=>{
+      if(!currentUser){
+      navigate("/login");
+      }
+    },[])
   return (
     <>
     <Navbar/>
