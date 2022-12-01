2d688618a59ca47dac2876f8f4bb100490eb831b
Laviz-pand <pandeylaviz@gmail.com>
2022-12-01 22:58:38 +0545

    Update

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -24,8 +24,6 @@ const NavContainerTop = styled.div`
 
 
     }
-
-
 `
 
 const NavLogo = styled.img`
--- src/Pages/ProfilePage.tsx
+++ src/Pages/ProfilePage.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const ProfilePageContainer = styled.div`
   height: 100vh;
-  width: 100vw;
+  width: 100%;
   @media(max-width: 1024px){
       width: 100%;
 
@@ -39,6 +39,7 @@ const Banner = styled.div`
     left: 0;
     top: 0;
     border-radius: 0;
+    object-fit: cover;
   }
 `
 const Name = styled.h2`
@@ -59,16 +60,34 @@ const Name = styled.h2`
     }
 
 `
+const DetailsContainer = styled.div`
+  position: absolute;
+  left: 150px;
+  top: 400px;
+  input{
+    background: none;
+    border: 3px solid;
+    border-color: ${props=>props.theme.primaryColor};
+    border-radius: 8px;
+    padding: 10px 20px;
+    color: white;
+  }
+
+`
 export default function ProfilePage() {
   return (
     <>
     <ProfilePageContainer>
       <Banner>
-        <img src="https://static.vecteezy.com/system/resources/thumbnails/007/623/233/small/3d-illustration-education-tablet-with-education-icons-on-on-hi-tech-background-education-banner-vector.jpg" alt="banner" />
+        <img src="https://assets-global.website-files.com/616e938268c8f0a92cb2b540/617806fb52d751a1fab3898a_youtube%20banner%20size%20guide.jpg" alt="banner" />
       </Banner>
       <img alt="profile" src='https://media-exp1.licdn.com/dms/image/C5603AQEyy_F49exDaQ/profile-displayphoto-shrink_800_800/0/1644306645936?e=1674691200&v=beta&t=h00G5kPe50cw3IJnh0rimzYXXoiMHd94jujwlJkuSHE'/>
       <Name>Laviz Pandey</Name>
     </ProfilePageContainer>
+    <DetailsContainer>
+      <input type="text" placeholder='Laviz Pandey'/>
+      <h2>kjasdas</h2>
+    </DetailsContainer>
     </>
   )
 }
