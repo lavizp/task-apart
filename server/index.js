2e8e7166c5ae0f179a83384e6e24ce49e42666d4
Laviz-pand <pandeylaviz@gmail.com>
2022-12-08 10:22:20 +0545

    Backend Setup

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+
+
+const app = express()
+dotenv.config()
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
