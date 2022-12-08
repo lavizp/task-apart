537c73368482ec056396e1adda362c2e74061c6d
Laviz-pand <pandeylaviz@gmail.com>
2022-12-08 12:19:07 +0545

    Updates

--- server/index.js
+++ server/index.js
@@ -2,12 +2,15 @@ import express from "express";
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 
-
 const app = express()
 dotenv.config()
-
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .then(() => app.listen(process.env.PORT, () => console.log(`Server Running on Port: http://localhost:${process.env.PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set('strictQuery', false);
+
+app.get("/", (req, res)=>{
+  res.status(200).send({asd: "asd"})
+})
+
