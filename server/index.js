d6c81c9ae1a6ac641037f1c165f2469658ee3d92
Laviz-pand <pandeylaviz@gmail.com>
2023-03-22 20:12:20 +0545

    Eslint Change

--- server/index.js
+++ server/index.js
@@ -1,24 +1,33 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import mongoose from "mongoose";
-import taskRouter from './routes/tasks.js'
-import userRouter from './routes/users.js'
-import cors from "cors"
+import taskRouter from "./routes/tasks.js";
+import userRouter from "./routes/users.js";
+import cors from "cors";
 import bodyParser from "body-parser";
-const app = express()
-dotenv.config()
+const app = express();
+dotenv.config();
 app.use(cors());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(process.env.PORT, () => console.log(`Server Running on Port: http://localhost:${process.env.PORT}`)))
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(
+        `Server Running on Port: http://localhost:${process.env.PORT}`
+      )
+    )
+  )
   .catch((error) => console.log(`${error} did not connect`));
 
-app.get("/", (req, res)=>{
-  res.status(200).send({asd: "asd"})
-})
+app.get("/", (req, res) => {
+  res.status(200).send({ asd: "asd" });
+});
 
-app.use("/tasks", taskRouter)
-app.use("/users", userRouter)
-
+app.use("/tasks", taskRouter);
+app.use("/users", userRouter);
